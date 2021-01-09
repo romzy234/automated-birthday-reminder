948428c6d68f153521deb9adbf77aba2958c35a2
Ogoh Cyril <44710924+romzy234@users.noreply.github.com>
2021-01-09 23:34:42 +0100

    Added Age To The Program

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var dateMonth = new Date().getMonth(); // start counting from 0
 var dateDay = new Date().getDate();// start counting from 1
 
 /* The Schema Which The Database Follow 
-
     {
         'id' : number,
         'name' : string,
@@ -15,7 +14,6 @@ var dateDay = new Date().getDate();// start counting from 1
         'email' : string 
     },
  * You can Use Any type of schema (This is the method I preferred)
-
 */
 
 /// database goes here 
@@ -23,25 +21,25 @@ var users = [
     {
         'id' : 000,
         'name' : 'user1',
-        'dob' : '14-6',
+        'dob' : '14-6-1994',
         'email' : 'user1@exapmle.com'
     },
     {
         'id' : 001,
         'name' : 'user2',
-        'dob' : '15-6',
+        'dob' : '15-6-2003',
         'email' : 'user2@exapmle.com'
     },
     {
         'id' : 002,
         'name' : 'user3',
-        'dob' : '17-4',
+        'dob' : '17-4-2004',
         'email' : 'user3@exapmle.com'
     },
     {
         'id' : 003,
         'name' : 'user4',
-        'dob' : '6-0',
+        'dob' : '6-0-1999',
         'email' : 'user4@exapmle.com'
     }
 ]
@@ -67,6 +65,7 @@ cron.schedule('* * 07 * * *', () => {
         let d = element.dob.split('-')
         let dM = +d[1]  // For the month
         let dD = +d[0] // for the day 
+        let age = dateYear - +d[2]
         console.log( typeof dM) //return number
         // Sending the Mail
         if(dateDay == dD && dateMonth == dM ){
@@ -74,7 +73,7 @@ cron.schedule('* * 07 * * *', () => {
                 from: 'YOUR EMAIL',
                 to: element.email,
                 subject: `Happy Birthday `,
-                html: `<b>Happy birthday ${element.name}</b>, Enjoy your day \n <small>this is auto generated</small>`                       
+                html: `Wishing You a <b>Happy birthday ${element.name}</b> On Your ${age}, Enjoy your day \n <small>this is auto generated</small>`                       
             };
             return transporter.sendMail(mailOptions, (error, data) => {
                 if (error) {
